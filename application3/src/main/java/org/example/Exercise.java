8e762052f5d5377e2aee3923abe1bbd30b36e6bf
Olena Zakharina <zakharina23@gmail.com>
2023-07-05 01:06:36 +0300

    added changes2

---
+++ application3/src/main/java/org/example/Exercise.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Exercise {
+
+    public Set<String> function1(String[]array){
+
+        ArrayList<String> SomeArray = new ArrayList<>();
+
+        for (String str : array) {
+
+            char[] myCharsArray = str.toCharArray();
+
+            for (int i = 0; i < str.length(); i++) {
+                for (int j = i + 1; j < str.length(); j++) {
+                    if (myCharsArray[i] == myCharsArray[j]) {
+
+                        String valueOfchar = String.valueOf(myCharsArray[j]);
+                        SomeArray.add(valueOfchar);
+
+                        break;
+                    }
+                }
+            }
+        }
+        Set<String> set = new LinkedHashSet<String>(SomeArray);
+        return set;
+    }
+
+}
